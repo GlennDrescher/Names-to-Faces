9038b4e17800c19e0f2507c5864966ff195c8a23
Glenn Drescher <GlennD@me.com>
2020-03-06 15:34:58 +0100

    Setup CollectionView

---
+++ Names to Faces/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  Names to Faces
+//
+//  Created by Glenn Drescher on 06/03/2020.
+//  Copyright © 2020 GDev.run. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var name: UILabel!
+    
+}
